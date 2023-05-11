b148981eff14d75a126219a19519a2e2df2b17e1
James Alves <james@jamesalves.com.br>
2023-05-10 21:16:05 -0300

    alterado

--- src/programa/Programa.java
+++ src/programa/Programa.java
@@ -3,5 +3,6 @@ package programa;
 public class Programa {
 public static void main(String[] args) {
 	System.out.println("############## compilado ##############");
+	System.out.println("Nova saida");
 }
 }
